1106fa8c04c65f584ad79c352552a1d644f3b68d
Cyber Kevin <emailkevinlarson@gmail.com>
2022-10-09 11:37:19 -0700

    Update WalletChat.jsx
    allow client hosted changes, or default to v1

--- src/WalletChat/WalletChat.jsx
+++ src/WalletChat/WalletChat.jsx
@@ -7,8 +7,7 @@ import { getCookie } from "../utils";
 export default function WalletChatWidget() {
   const [isOpen, setIsOpen] = useState(false);
   const [numUnread, setNumUnread] = useState(0);
-  // const url = process.env.REACT_APP_APP_URL || "http://localhost:3000"  //"https://app.walletchat.fun/"//"app.walletchat.fun" //http://localhost:3000
-  const url = "http://192.168.50.30:3000" || "http://localhost:3000";
+  const url = process.env.REACT_APP_APP_URL || "https://v1.walletchat.fun"
   console.log(`url: ${url}`);
   const clickHandler = (e) => {
     setIsOpen(!isOpen);
